f8eee86e77ca623d3bb4cda3c141c91f0de39872
rishabhrawat692-source <rishabhrawat692@gmail.com>
2026-08-21 19:19:32 +0530

    Update 01_basic_sql.sql

--- sql_queries/01_basic_sql.sql
+++ sql_queries/01_basic_sql.sql
@@ -1,2 +1,32 @@
 -- Employee Salary & Department Analysis
 -- Basic SQL Queries
+-- Employee Salary & Department Analysis
+-- Basic SQL Queries
+
+-- Q1. Display all employees
+SELECT *
+FROM employee;
+
+-- Q2. Display employee names and salaries
+SELECT name, salary
+FROM employee;
+
+-- Q3. Find employees with salary greater than 50,000
+SELECT name, salary
+FROM employee
+WHERE salary > 50000;
+
+-- Q4. Find employees from Delhi
+SELECT name, city
+FROM employee
+WHERE city = 'Delhi';
+
+-- Q5. Display employees in descending order of salary
+SELECT name, salary
+FROM employee
+ORDER BY salary DESC;
+
+-- Q6. Display the first 5 employees
+SELECT *
+FROM employee
+LIMIT 5;
