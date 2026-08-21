32f7ccde18ae0db1f84672cf452b55ef50756fe7
rishabhrawat692-source <rishabhrawat692@gmail.com>
2026-08-21 19:23:51 +0530

    Update 02_aggregate_functions.sql

--- sql_queries/sql_queries/02_aggregate_functions.sql
+++ sql_queries/sql_queries/02_aggregate_functions.sql
@@ -1,2 +1,21 @@
 -- Employee Salary & Department Analysis
 -- Aggregate Functions
+-- Q7. Count total employees
+SELECT COUNT(*) AS employee_count
+FROM employee;
+
+-- Q8. Find total salary
+SELECT SUM(salary) AS total_salary
+FROM employee;
+
+-- Q9. Find average salary
+SELECT AVG(salary) AS average_salary
+FROM employee;
+
+-- Q10. Find highest salary
+SELECT MAX(salary) AS highest_salary
+FROM employee;
+
+-- Q11. Find lowest salary
+SELECT MIN(salary) AS lowest_salary
+FROM employee;
