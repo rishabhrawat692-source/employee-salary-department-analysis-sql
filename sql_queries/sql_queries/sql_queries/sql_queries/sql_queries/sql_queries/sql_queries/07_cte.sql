eb4e953812c08fa0c93771d699e05b064e1bdc98
rishabhrawat692-source <rishabhrawat692@gmail.com>
2026-08-21 19:32:14 +0530

    Update 07_cte.sql

--- sql_queries/sql_queries/sql_queries/sql_queries/sql_queries/sql_queries/sql_queries/07_cte.sql
+++ sql_queries/sql_queries/sql_queries/sql_queries/sql_queries/sql_queries/sql_queries/07_cte.sql
@@ -1,2 +1,20 @@
 -- Employee Salary & Department Analysis
 -- CTE Queries
+
+-- Q31. Find the average salary using a CTE
+WITH avg_salary AS (
+    SELECT AVG(salary) AS average_salary
+    FROM employee
+)
+SELECT *
+FROM avg_salary;
+
+-- Q32. Find employees earning more than the average salary using a CTE
+WITH avg_salary AS (
+    SELECT AVG(salary) AS average_salary
+    FROM employee
+)
+SELECT employee.name, employee.salary
+FROM employee
+CROSS JOIN avg_salary
+WHERE employee.salary > avg_salary.average_salary;
